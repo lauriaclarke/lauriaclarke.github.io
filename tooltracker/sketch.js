aaaa8afbb7aeb34e4b7d598a326aa4ef0e0a274d
Lauria <lauria.clarke@gmail.com>
2021-10-04 10:37:08 -0400

    added iframe for tool tracker

--- tooltracker/sketch.js
+++ tooltracker/sketch.js
@@ -2,9 +2,11 @@
 var canvasX = 4000;
 var canvasY = 600;
 var nDays = 55;
+var onoff = true;
 
 function setup(){
   createCanvas(canvasX, canvasY);
+  background(202);
   textAlign(CENTER, CENTER);
 }
 
@@ -14,16 +16,19 @@ function draw() {
   var x2 = 0 + 100;
   var y2 = canvasY / 2;
 
-  increment  = (canvasX - 100 )/ (nDays + 1); 
-  console.log(toolUse.length);
-  for(let i = 0; i < toolUse.length; i++){
-    line(x1, y1, x2, y2);
-    ellipse(x2, y2, 5, 5);
-    printTools(x2, i);
-    x2 = x1
-    x1 += increment;
+  if(onoff){
+    text( "this is a list of the tools I have used on a daily basis over the 55 days between August 1st and September 26th", 800, 100);
+    increment = (canvasX - 100 )/ (nDays + 1); 
+    // console.log(toolUse.length);
+    for(let i = 0; i < toolUse.length; i++){
+      line(x1, y1, x2, y2);
+      ellipse(x2, y2, 5, 5);
+      printTools(x2, i);
+      x2 = x1
+      x1 += increment;
+    }
+    onoff = false;
   }
-  background();
 }
 
 function printTools(x, index){
